7e1c8788d91ed5430d74923a2353721386a5381e
devkartikrathi <kartikrathi0808@gmail.com>
2023-01-31 11:20:32 +0530

    ramdom file deleted

--- combined/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-plt.plot(df['itr'], df['m'], color='pink', label='Merge Sort')
-plt.plot(df['itr'], df['q'], color='orange', label='Quick Sort')
-plt.plot(df['itr'], df['h'], color='cyan', label='Heap Sort')
-plt.legend()
